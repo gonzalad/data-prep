57755ac7f642c4a11fd43922b585d0073f3b66c7
Nicolas Comont <ncomont@talend.com>
2018-01-04 11:39:27 +0100

    fix: binding

--- dataprep-webapp/src/app/components/widgets/button-loader/widget-button-loader-directive.js
+++ dataprep-webapp/src/app/components/widgets/button-loader/widget-button-loader-directive.js
@@ -37,7 +37,7 @@ export default function TalendButtonLoader() {
 		templateUrl: template,
 		scope: {
 			buttonClass: '@',
-			disableCondition: '<',
+			disableCondition: '=',
 			label: '@',
 			loading: '=',
 			tooltipLabel: '@title',
